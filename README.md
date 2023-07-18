38240d9afca7cc1d72ee88471f18931b4f64e8e0
Lütfü <mogretici@yandex.com.tr>
2023-07-19 00:53:30 +0300

    screenschotsAdded

--- README.md
+++ README.md
@@ -4,6 +4,9 @@
 
 [![NPM](https://img.shields.io/npm/v/xfoxui.svg)](https://www.npmjs.com/package/xfoxui) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
 
+| [![PRIMARY](screenshots/screenshot-1.png)](screenshots/screenshot-1.png) | [![SECONDARY](screenshots/screenshot-2.png)](screenshots/screenshot-2.png) | [![DASHED](screenshots/screenshot-3.png)](screenshots/screenshot-3.png) | [![LINK](screenshots/screenshot-4.png)](screenshots/screenshot-4.png) |
+| ------------------------------------------------------------------------ | -------------------------------------------------------------------------- | ----------------------------------------------------------------------- | --------------------------------------------------------------------- |
+
 ## Install
 
 ```bash
@@ -21,10 +24,10 @@ import 'xfoxui/dist/index.css'
 class Example extends Component {
   render() {
     return (
-    <Button type='primary' text="Click Me" /> <br />
-    <Button type='secondary' text="Click Me" /> <br />
-    <Button type='dashed'  text="Click Me" /> <br />
-    <Button type='link' text="Click Me" /> <br />
+    <Button type='primary' text="PRIMARY" /> <br />
+    <Button type='secondary' text="SECONDARY" /> <br />
+    <Button type='dashed'  text="DASHED" /> <br />
+    <Button type='link' text="LINK" /> <br />
 
     )
   }
--- example/src/App.js
+++ example/src/App.js
@@ -5,12 +5,35 @@ import 'foxui/dist/index.css'
 
 const App = () => {
   return (
-    <div>
-    <Button type='primary' onClick={()=>alert("https://github.com/mogretici/xfoxui")} text="Click Me" /> <br />
-    <Button type='secondary' onClick={()=>alert("https://github.com/mogretici/xfoxui")} text="Click Me" /> <br />
-    <Button type='dashed' onClick={()=>alert("https://github.com/mogretici/xfoxui")} text="Click Me" /> <br />
-    <Button type='link' onClick={()=>alert("https://github.com/mogretici/xfoxui")} text="Click Me" /> <br />
-
+    <div className='container'>
+      <div className='backgroundImage'></div>
+      <div className='content'>
+        <h1 className='headText'>xFOXUI Buttons</h1>
+        <Button
+          type='primary'
+          onClick={() => alert('https://github.com/mogretici/xfoxui')}
+          text='PRIMARY'
+        />
+        <br />
+        <Button
+          type='secondary'
+          onClick={() => alert('https://github.com/mogretici/xfoxui')}
+          text='SECONDARY'
+        />
+        <br />
+        <Button
+          type='dashed'
+          onClick={() => alert('https://github.com/mogretici/xfoxui')}
+          text='DASHED'
+        />
+        <br />
+        <Button
+          type='link'
+          onClick={() => alert('https://github.com/mogretici/xfoxui')}
+          text='LINK'
+        />
+        <br />
+      </div>
     </div>
   )
 }
