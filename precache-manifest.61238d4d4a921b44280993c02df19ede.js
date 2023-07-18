c2612752624c83bd8c029408b08fe405ee6727ee
Lütfü <mogretici@yandex.com.tr>
2023-07-19 00:31:33 +0300

    Updates

---
+++ precache-manifest.61238d4d4a921b44280993c02df19ede.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a34c1ceef39bc5825b38f23afd039ac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3bc98df98be2b7931803",
+    "url": "./static/css/main.e1066e18.chunk.css"
+  },
+  {
+    "revision": "864f6abe7943756b4026",
+    "url": "./static/js/2.320d38e3.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.320d38e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc98df98be2b7931803",
+    "url": "./static/js/main.59aab693.chunk.js"
+  },
+  {
+    "revision": "adc2f36e93fff8f7605e",
+    "url": "./static/js/runtime-main.754bce60.js"
+  },
+  {
+    "revision": "dc8a82bf262732378cf606b3c8143cc5",
+    "url": "./static/media/fox.dc8a82bf.png"
+  }
+]);
